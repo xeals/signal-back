6c174c9c30e682a99ea2ac8822fd2b87e7cea376
xeals <alex.smith@owari.cc>
2018-04-03 16:38:31 +1000

    Port u32 as byte array functions

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,25 @@
+#[allow(dead_code)]
 pub fn array_as_u32(array: &[u8; 4]) -> u32 {
     u32::from(array[0]) << 24 | u32::from(array[1]) << 16
         | u32::from(array[2]) << 8 | u32::from(array[3])
+}
+
+#[allow(dead_code)]
+pub fn u32_as_vec(n: u32) -> Vec<u8> {
+    let mut arr = Vec::with_capacity(4);
+    arr[0] = (n >> 24) as u8;
+    arr[1] = (n >> 16) as u8;
+    arr[2] = (n >> 8) as u8;
+    arr[3] = n as u8;
+    arr
+}
+
+#[allow(dead_code)]
+pub fn u32_into_vec(dest: &mut Vec<u8>, n: u32) {
+    dest[0] = (n >> 24) as u8;
+    dest[1] = (n >> 16) as u8;
+    dest[2] = (n >> 8) as u8;
+    dest[3] = n as u8;
 }
 
 /// Pads a `Vec` with 0s to meet the desired length.
