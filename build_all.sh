ccb9dd8b54f68faed694e5f0251603ee9f185e14
xeals <alex.smith@owari.cc>
2018-04-08 14:54:32 +1000

    forgot to add build script for travis

---
+++ build_all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_OSS="linux windows darwin"
+BUILD_ARCHS="386 amd64"
+BUILD_ROOT="$(git rev-parse --show-toplevel)"
+#BUILD_VER="$(git describe --tags --dirty)"
+BUILD_VER="0.0.0"
+
+if [[ "$(pwd)" != "${BUILD_ROOT}" ]] ; then
+    echo "must be in build root '${BUILD_ROOT}'"
+    exit 1
+fi
+
+if [[ ! -d "${BUILD_ROOT}/release" ]] ; then
+    mkdir -p "${BUILD_ROOT}/release"
+fi
+
+for OS in ${BUILD_OSS[@]} ; do
+    for ARCH in ${BUILD_ARCHS[@]} ; do
+        NAME="signal-back_${OS}_${ARCH}"
+        echo "Building for ${OS}/${ARCH}"
+
+        GOOS=$OS GOARCH=$ARCH go build -o "${BUILD_ROOT}/release/${NAME}" .
+        shasum -a 256 "${BUILD_ROOT}/release/${NAME}" > "${BUILD_ROOT}/release/${NAME}.sha256"
+    done
+done
